a2c1330cd3dc596af69f7cf5c497513ed10c98f7
juan coelho <juancoelho25@hotmail.com>
2024-05-31 13:42:40 -0300

    feat: ad screen created

--- frontend/src/pages/Advertisement/Advertisement.tsx
+++ frontend/src/pages/Advertisement/Advertisement.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
-
-import Menu from '../../components/Menu/Menu'
+import React from 'react';
+import Menu from '../../components/Menu/Menu';
+import './Advertisement.css';
+import ad from '../../public/advertisement/advertisement.svg';
 
 const Advertisement = () => {
   return (
-    <div>
-        <Menu></Menu>
+    <div className="advertisement-container">
+      <Menu />
+      <div className="ad-screen">
+        <img src={ad} alt="ad-logo" />
+        Anúncios
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Advertisement
+export default Advertisement;
