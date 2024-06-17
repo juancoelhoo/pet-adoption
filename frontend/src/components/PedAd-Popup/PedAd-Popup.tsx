0811cf204501640c562b5322da05a9b9a77df464
Juan Coelho <juancoelho25@hotmail.com>
2024-06-16 21:01:45 -0300

    feat: profile access added

--- frontend/src/components/PedAd-Popup/PedAd-Popup.tsx
+++ frontend/src/components/PedAd-Popup/PedAd-Popup.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './PedAd-Popup.css';
 import close from '../../public/pet-ad/close-btn.svg';
 import contato from '../../public/pet-ad/whatsapp.svg';
+import profile from '../../public/menu/profile.svg';
+import profileAccess from '../../public/pet-ad/profile-access.svg'; // Assuming this is the correct import for profileAccess
 
 interface PedAdPopupProps {
   trigger: boolean;
@@ -24,10 +26,17 @@ const PedAdPopup: React.FC<PedAdPopupProps> = ({ trigger, onClose, name, breed, 
           <p><strong>Raça:</strong> {breed}</p>
           <p><strong>Idade:</strong> {age} anos</p>
           <p><strong>Descrição:</strong> {description}</p>
+          <div className="profile">
+            <div className="profile-user">
+            <img src={profile} alt="profile-photo" />
+            <span>Stephanie</span>
+            </div>
+            <img src={profileAccess} alt="profile-access" className="profile-access" />
+          </div>
           <button className='contact'>
             Converse com o Dono
             <img src={contato} alt="whatsapp-image" className='btn-image'/>
-            </button>
+          </button>
         </div>
       </div>
     </div>
