4eb49d6d703b0302d7451b819ae0c91cbc622cc2
beviggiano <beviggiano@gmail.com>
2024-06-18 07:56:58 -0300

    fix: using axios undirectly

--- frontend/src/components/PetAd/PetAd.tsx
+++ frontend/src/components/PetAd/PetAd.tsx
@@ -1,19 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { api } from '../../services/api';
 import './PetAd.css';
 import dogimage from '../../public/pet-ad/pet-ad.svg';
 import nameimage from '../../public/pet-ad/pet-name.svg';
 import breedimage from '../../public/pet-ad/pet-breed.svg';
 import ageimage from '../../public/pet-ad/pet-age.svg';
 import descimage from '../../public/pet-ad/pet-description.svg';
-import likeimage from '../../public/pet-ad/like.svg'; // Importe a imagem de curtida
+import likeimage from '../../public/pet-ad/like.svg'; 
 
 function PetAd() {
   const [likes, setLikes] = useState(0);
 
-  // Fetch initial likes from the backend when the component mounts
   useEffect(() => {
-    axios.get('http://localhost:3333/reactions')
+    api.get('/reactions')
       .then(response => {
         setLikes(response.data.likes);
       })
@@ -22,12 +21,11 @@ function PetAd() {
       });
   }, []);
 
-  // Handle the like button click
   const handleLike = async () => {
     try {
-      const response = await axios.post('http://localhost:3333/reactions/toggleLike', {
-        userId: 1, // Substitua pelo userId real
-        postId: 1  // Substitua pelo postId real
+      const response = await api.post('/reactions/toggleLike', {
+        userId: 1, 
+        postId: 1  
       });
       setLikes(response.data.likes);
     } catch (error) {
