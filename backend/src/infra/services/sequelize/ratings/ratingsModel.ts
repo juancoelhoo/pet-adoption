8a8fddd0c15827647bf1533b6c0e1230ada47e5e
beviggiano <beviggiano@gmail.com>
2024-06-23 17:01:26 -0300

    feat: ratingsModel implemented

---
+++ backend/src/infra/services/sequelize/ratings/ratingsModel.ts
@@ -0,0 +1,33 @@
+import { Column, ForeignKey, BelongsTo, Model, PrimaryKey, Table, AutoIncrement } from "sequelize-typescript";
+import { UserModel } from "../users/usersModel";
+
+@Table({
+  tableName: 'ratings',
+  timestamps: false
+})
+export class RatingModel extends Model {
+    @PrimaryKey
+    @AutoIncrement
+    @Column({ allowNull: false })
+    id: number;
+
+    @Column({ allowNull: false })
+    grade: number;
+
+    @ForeignKey(() => UserModel)
+    @Column({ allowNull: false, onDelete: "CASCADE" })
+    reporter_user_id: number;
+
+    @ForeignKey(() => UserModel)
+    @Column({ allowNull: false, onDelete: "CASCADE" })
+    reported_user_id: number;
+
+    @Column({ allowNull: false })
+    created_at: Date;
+
+    @BelongsTo(() => UserModel, 'reporter_user_id')
+    reporter: UserModel;
+
+    @BelongsTo(() => UserModel, 'reported_user_id')
+    reported: UserModel;
+}
